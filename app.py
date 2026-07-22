c36dda90f667e6f52c6060200b3c72f44a3fb7e8
Kashif Anwar <anwarkashif@outlook.com>
2026-07-23 04:00:16 +0530

    SD Bot Test 4

--- app.py
+++ app.py
@@ -225,8 +225,8 @@ if st.session_state['role'] is None:
         if root_dir not in sys.path:
             sys.path.insert(0, root_dir)
             
-        from extractor import ExtractorNode
-        from analyst import AnalystNode
+        from Nodes.extractor import ExtractorNode
+        from Nodes.analyst import AnalystNode
         
         extractor = ExtractorNode()
         analyst = AnalystNode()
